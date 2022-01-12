4ca004864bc056ba25da0e6b4d6a2804e2ee6458
Stas Makarov <stas.makarov.90@gmail.com>
2022-01-12 10:51:30 +0100

    kinda working app

--- src/main/keechma101/app.cljs
+++ src/main/keechma101/app.cljs
@@ -1,8 +1,55 @@
-(ns keechma101.app)
+(ns keechma101.app
+  (:require [keechma.next.core :as core]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]
 
-(-> js/document
-    (.getElementById "app")
-    (.-innerHTML)
-    (set! "Hello Clojure!"))
+            [keechma101.controllers]))
 
-(prn "HW")
+(defn- page= [page]
+  (fn [router-ctr-data]
+    (when (= page (-> router-ctr-data :router :data :name))
+      router-ctr-data)))
+
+(def app
+  {:keechma/controllers
+   {:router {:keechma.controller/params true}
+
+    :profile {:keechma.controller/deps [:router]
+              :keechma.controller/params (page= :page/profile)}
+
+    :books-list  {:keechma.controller/deps   [:router]
+                  :keechma.controller/params (page= :page/books-list)}
+
+    :books-show  {:keechma.controller/deps   [:router]
+                  :keechma.controller/params (page= :page/books-show)}
+    }})
+
+(def app-instance (core/start! app))
+
+(def routes
+  ["/"
+   ["books" {:name :page/books-list}]
+   ["books/{id}" {:name :page/books-show}]
+   ["profile" {:name :page/profile}]])
+
+(def router (rf/router routes))
+
+(defn on-navigate [new-match]
+  (prn "IN on-navigate " new-match)
+  (core/dispatch app-instance :router new-match))
+
+(defn init-router! []
+  (rfe/start! router on-navigate {:use-fragment true}))
+
+(defn ^:export main []
+  (init-router!))
+
+(comment
+  (core/get-derived-state app-instance)                                ;; {:counter 0}
+
+  (core/dispatch app-instance :router :books123)
+
+  (core/get-derived-state app-instance)                                ;; {:counter 1}
+
+  (core/subscribe app-instance :router #(prn "IN SUB HANDLER " %))
+  )
---
+++ src/main/keechma101/db.cljs
@@ -0,0 +1,11 @@
+(ns keechma101.db)
+
+(def books
+  [{:id     1
+    :title  "High Performance Browser Networking"
+    :author "Ilya Grigorik"}
+   {:id     2
+    :title  "Designing Data-Intensive Applications"
+    :author "Martin Kleppmann "}])
+
+(def user {:name "Joe" :email "joe@gma.il"})
